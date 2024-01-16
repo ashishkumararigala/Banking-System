197076eeb7e59d1d535e70113f98755992a407f4
Arigala Ashish Kumar <156767659+ashishkumararigala@users.noreply.github.com>
2024-01-16 12:32:49 +0530

    Created savings_account.py

---
+++ savings_account.py
@@ -0,0 +1,9 @@
+class SavingsAccount(Account):
+    def __init__(self, account_holder, account_number, balance=0, interest_rate=0.02):
+        super().__init__(account_holder, account_number, balance)
+        self.interest_rate = interest_rate
+
+    def add_interest(self):
+        interest = self.balance * self.interest_rate
+        self.balance += interest
+        print(f"Added interest. Current balance: ${self.balance}")
